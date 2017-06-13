0bd67838d50a0a8defc59e61cef26888f3154e42
Joseph Simone <jsimone@dealcoachpro.com>
2017-06-13 19:00:16 -0400

    update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -4,80 +4,47 @@
 #
 # Sample output:
 #
-# ./rockpaperscissors.py
+# ./rockpaperscissors.py 
 # Player 1 choose rock, paper or scissors? rock
+# Player 1 chooses rock
 # Player 2 choose rock, paper or scissors? paper
-# Player 2 wins because paper covers rock
+# Player 2 chooses paper
+# Paper covers rock. Player 2 wins
 #
-# ./rockpaperscissors.py
-# Player 1 choose rock, paper or scissors? paper
-# Player 2 choose rock, paper or scissors? rock
-# Player 1 wins because paper covers rock
-#
-# ./rockpaperscissors.py
-# Player 1 choose rock, paper or scissors? scissors
-# Player 2 choose rock, paper or scissors? rock
-# Player 2 wins because rock smashes scissors
-#
-# ./rockpaperscissors.py
+# ./rockpaperscissors.py 
 # Player 1 choose rock, paper or scissors? rock
-# Player 2 choose rock, paper or scissors? scissors
-# Player 1 wins because rock smashes scissors
-#
-# ./rockpaperscissors.py
-# Player 1 choose rock, paper or scissors? scissors
-# Player 2 choose rock, paper or scissors? paper
-# Player 1 wins because scissors cuts paper
-#
-# ./rockpaperscissors.py
-# Player 1 choose rock, paper or scissors? paper
-# Player 2 choose rock, paper or scissors? scissors
-# Player 2 wins because scissors cuts paper
-#
-# ./rockpaperscissors.py
-# Player 1 choose rock, paper or scissors? rock
+# Player 1 chooses rock
 # Player 2 choose rock, paper or scissors? rock
-# Player 1 ties with player 2. They both played rock
-#
-# ./rockpaperscissors.py
-# Player 1 choose rock, paper or scissors? scissors
-# Player 2 choose rock, paper or scissors? scissors
-# Player 1 ties with player 2. They both played scissors
-#
-# ./rockpaperscissors.py
-# Player 1 choose rock, paper or scissors? paper
-# Player 2 choose rock, paper or scissors? paper
-# Player 1 ties with player 2. They both played paper
-
+# Player 2 chooses rock
+# It's a tie. Player 1 and 2 both played rock
 
 import sys
 if sys.version_info[0] < 3:
 	raise Exception("Must be using python 3")
-    
-valid_choices = {'rock', 'paper', 'scissors'}  # create a python set of all possible valid choices
 
 def getPlayerInput(player):
-    response = input(str(player)+' choose rock, paper or scissors? ')
-    response = response.strip().lower()    # strip out whitespace and convert to lowercase
-    while response not in valid_choices:
+    response = ''
+    while response not in {'rock', 'paper', 'scissors'}:
         response = input(str(player)+' choose rock, paper or scissors? ')
         response = response.strip().lower()    # strip out whitespace and convert to lowercase
     return response
     
 player1 = getPlayerInput('Player 1')
+print('Player 1 chooses '+str(player1))
 player2 = getPlayerInput('Player 2')
+print('Player 2 chooses '+str(player2))
 
 if player1 == 'rock' and player2 == 'scissors':
-    print("Player 1 wins because rock smashes scissors")
+    print("Rock smashes scissors. Player 1 wins.")
 elif player1 == 'scissors' and player2 == 'rock':
-    print("Player 2 wins because rock smashes scissors")
+    print("Rock smashes scissors. Player 2 wins.")
 elif player1 == 'paper' and player2 == 'rock':
-    print("Player 1 wins because paper covers rock")
+    print("Paper covers rock.  Player 1 wins.")
 elif player1 == 'rock' and player2 == 'paper':
-    print("Player 2 wins because paper covers rock")
+    print("Paper covers rock. Player 2 wins")
 elif player1 == 'scissors' and player2 == 'paper':
-    print("Player 1 wins because scissors cuts paper")
+    print("Scissors cuts paper.  Player 1 wins.")
 elif player1 == 'paper' and player2 == 'scissors':
-    print("Player 2 wins because scissors cuts paper")
+    print("Scissors cuts paper.  Player 2 wins")
 else:
-    print("Player 1 ties with player 2. They both played "+player1)
+    print("It's a tie. Player 1 and 2 both played "+player1)
